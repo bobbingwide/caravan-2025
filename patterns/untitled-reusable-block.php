40a94e92c95eaf20b315141c1f790e94a31a4ebc
Herb Miller <herb@bobbingwide.com>
2024-12-17 11:15:47 +0000

    Add several files. Not sure why though! #1

---
+++ patterns/untitled-reusable-block.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Title: Untitled Reusable Block
+ * Slug: caravan-2025/untitled-reusable-block
+ * Categories: 
+ */
+?>
+<!-- wp:paragraph -->
+<p><a href="https://caravan.lnk.to/DeccaDeramYears">https://caravan.lnk.to/DeccaDeramYears</a></p>
+<!-- /wp:paragraph -->
